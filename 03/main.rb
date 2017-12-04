937ea367a80a432d3b7ee3d38796b302d56f2e0e
Nicholas Yang <ny585@nyu.edu>
2017-12-03 23:05:48 -0500

    welp, did half in Rust half in Ruby. Guess that counts?

---
+++ 03/main.rb
@@ -0,0 +1,64 @@
+grid = []
+
+1000.times.each do |i|
+  grid[i] = Array.new(1000, 0)
+end
+
+x = 500
+y = 500
+direction = "right"
+is_turning = false
+path_length = 1
+steps_taken = 0
+times = 0
+
+grid[x][y] = 1
+
+100.times.each do |i|
+  grid[x][y] = grid[x + 1][y] +
+               grid[x - 1][y] +
+               grid[x][y + 1] +
+               grid[x][y - 1] +
+               grid[x + 1][y + 1] +
+               grid[x - 1][y + 1] +
+               grid[x + 1][y - 1] +
+               grid[x - 1][y - 1] +
+               grid[x][y]
+  puts grid[x][y] if grid[x][y] > 312051
+  if is_turning
+    case direction
+    when "right"
+      direction = "up"
+    when "up"
+      direction = "left"
+    when "left"
+      direction = "down"
+    when "down"
+      direction = "right"
+    end
+    is_turning = false
+  end
+
+  case direction
+  when "right"
+    x = x + 1
+  when "up"
+    y = y + 1
+  when "left"
+    x = x - 1
+  when "down"
+    y = y - 1
+  end
+
+  
+  steps_taken = steps_taken + 1
+  if steps_taken == path_length
+    steps_taken = 0
+    is_turning = true
+    times = times + 1
+    if times == 2
+      times = 0
+      path_length = path_length + 1
+    end
+  end
+end
---
+++ 03/main.rs
@@ -0,0 +1,11 @@
+
+fn main() {
+    let mut grid: Vec<Vec<i32>> = Vec::new();
+    for _i in 0..99 {
+        grid.push(vec![0; 100]);
+    }
+
+    for i in 0..99 {
+        println!("{}", )
+    }
+}
